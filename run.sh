084d0746f2398cceda6c73f641d6cead4b316da5
Iain Mosima <iainmosima@gmail.com>
2025-03-05 16:24:30 +0300

    update default environment in run script from pension to it

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Default to pension if no environment is specified
-ENV=${1:-pension}
+ENV=${1:-it}
 
 echo "Running bulk email sender with environment: $ENV"
 python bulk-email-sender.py --env $ENV
